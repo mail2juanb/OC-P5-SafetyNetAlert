145a647f0c2153715bf7007febeb9075e96ac3a2
juanb <mail2juanb@yahoo.fr>
2024-07-21 10:46:52 +0200

    Initial commit

---
+++ src/test/java/com/oc_P5/SafetyNetAlerts/SafetyNetAlertsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc_P5.SafetyNetAlerts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SafetyNetAlertsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
