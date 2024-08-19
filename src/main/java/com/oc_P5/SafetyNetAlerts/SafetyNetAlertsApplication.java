48bd4f73fb78ad502328cd38bec12f3ecc004e10
juanb <mail2juanb@yahoo.fr>
2024-08-19 17:12:21 +0200

    STYLE - Little CleanUp SafetyNetAlertsApplication.java

--- src/main/java/com/oc_P5/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/oc_P5/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -17,39 +17,40 @@ public class SafetyNetAlertsApplication {
 
         SpringApplication.run(SafetyNetAlertsApplication.class, args);
 
-
         log.info("Loading datas ... ");
 
         ObjectMapper objectMapper = new ObjectMapper();
+
         // Lire le fichier JSON et le convertir en objet DataWrapper
         DataWrapper dataWrapper = objectMapper.readValue(new File("src/main/resources/data.json"), DataWrapper.class);
-        log.info("data loaded : {} persons, {} stations and  {} medical records", dataWrapper.getPersons().size() , dataWrapper.getFireStations().size(), dataWrapper.getMedicalRecords().size());
-//
-//
-//        // Afficher les details de l'objet désérialisé du node persons
-//        for (Person person : dataWrapper.getPersons()) {
-//            log.debug("Name: " + person.getFirstName() + " " + person.getLastName());
-//            log.debug("Address: " + person.getAddress());
-//            log.debug("City: " + person.getCity());
-//            log.debug("Zip: " + person.getZip());
-//            log.debug("Phone: " + person.getPhone());
-//            log.debug("Email: " + person.getEmail());
-//        }
-//
-//        // Afficher les details de l'objet désérialisé du node firestations
-//        for (Firestation firestations : dataWrapper.getFirestations()) {
-//            log.debug("Address: " + firestations.getAddress());
-//            log.debug("Station: " + firestations.getStation());
-//        }
-//
-//        // Afficher les details de l'objet désérialisé du node medicalrecords
-//        for (Medicalrecord medicalrecord : dataWrapper.getMedicalrecords()) {
-//            log.debug("Name: " + medicalrecord.getFirstName() + " " + medicalrecord.getLastName());
-//            log.debug("Birthdate: " + medicalrecord.getBirthdate());
-//            log.debug("Medications: " + String.join(", ", medicalrecord.getMedications()));
-//            log.debug("Allergies: " + String.join(", ", medicalrecord.getAllergies()));
-//        }
 
+        log.info("data loaded : {} persons, {} stations and  {} medical records", dataWrapper.getPersons().size() , dataWrapper.getFireStations().size(), dataWrapper.getMedicalRecords().size());
+
+/*
+        Afficher les details de l'objet désérialisé du node persons
+        for (Person person : dataWrapper.getPersons()) {
+            log.debug("Name: " + person.getFirstName() + " " + person.getLastName());
+            log.debug("Address: " + person.getAddress());
+            log.debug("City: " + person.getCity());
+            log.debug("Zip: " + person.getZip());
+            log.debug("Phone: " + person.getPhone());
+            log.debug("Email: " + person.getEmail());
+        }
+
+        Afficher les details de l'objet désérialisé du node firestations
+        for (Firestation firestations : dataWrapper.getFirestations()) {
+            log.debug("Address: " + firestations.getAddress());
+            log.debug("Station: " + firestations.getStation());
+        }
+
+        Afficher les details de l'objet désérialisé du node medicalrecords
+        for (Medicalrecord medicalrecord : dataWrapper.getMedicalrecords()) {
+            log.debug("Name: " + medicalrecord.getFirstName() + " " + medicalrecord.getLastName());
+            log.debug("Birthdate: " + medicalrecord.getBirthdate());
+            log.debug("Medications: " + String.join(", ", medicalrecord.getMedications()));
+            log.debug("Allergies: " + String.join(", ", medicalrecord.getAllergies()));
+        }
+*/
 
     }
 
