8d3c59b8d0638b8ca742d0b947a1238f39adc640
juanb <mail2juanb@yahoo.fr>
2024-08-20 12:43:21 +0200

    STYLE - Get personal notes in commentary

--- src/main/java/com/oc_P5/SafetyNetAlerts/SafetyNetAlertsApplication.java
+++ src/main/java/com/oc_P5/SafetyNetAlerts/SafetyNetAlertsApplication.java
@@ -26,6 +26,31 @@ public class SafetyNetAlertsApplication {
 
         log.info("data loaded : {} persons, {} stations and  {} medical records", dataWrapper.getPersons().size() , dataWrapper.getFireStations().size(), dataWrapper.getMedicalRecords().size());
 
+        /*
+        // Afficher les details de l'objet désérialisé du node persons
+        for (Person person : dataWrapper.getPersons()) {
+            log.debug("Name: " + person.getFirstName() + " " + person.getLastName());
+            log.debug("Address: " + person.getAddress());
+            log.debug("City: " + person.getCity());
+            log.debug("Zip: " + person.getZip());
+            log.debug("Phone: " + person.getPhone());
+            log.debug("Email: " + person.getEmail());
+        }
+
+        // Afficher les details de l'objet désérialisé du node firestations
+        for (Firestation firestations : dataWrapper.getFirestations()) {
+            log.debug("Address: " + firestations.getAddress());
+            log.debug("Station: " + firestations.getStation());
+        }
+
+        // Afficher les details de l'objet désérialisé du node medicalrecords
+        for (Medicalrecord medicalrecord : dataWrapper.getMedicalrecords()) {
+            log.debug("Name: " + medicalrecord.getFirstName() + " " + medicalrecord.getLastName());
+            log.debug("Birthdate: " + medicalrecord.getBirthdate());
+            log.debug("Medications: " + String.join(", ", medicalrecord.getMedications()));
+            log.debug("Allergies: " + String.join(", ", medicalrecord.getAllergies()));
+        }
+        */
     }
 
 }
