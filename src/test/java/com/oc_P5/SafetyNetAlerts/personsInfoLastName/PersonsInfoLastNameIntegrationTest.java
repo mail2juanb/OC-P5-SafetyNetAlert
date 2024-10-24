16ecf30ba7b874a0bf8cfac846a7f65f876d74c7
juanb <mail2juanb@yahoo.fr>
2024-10-24 23:12:29 +0200

    ADD - @DirtiesContext in Integration Tests

--- src/test/java/com/oc_P5/SafetyNetAlerts/personsInfoLastName/PersonsInfoLastNameIntegrationTest.java
+++ src/test/java/com/oc_P5/SafetyNetAlerts/personsInfoLastName/PersonsInfoLastNameIntegrationTest.java
@@ -10,6 +10,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
@@ -25,6 +26,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ExtendWith(SpringExtension.class)
 @SpringBootTest
 @AutoConfigureMockMvc
+@DirtiesContext
 public class PersonsInfoLastNameIntegrationTest {
 
 
