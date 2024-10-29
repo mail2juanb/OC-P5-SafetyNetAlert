36bd5a9078ff4e52cac1ba5f3a83f183bb7404e4
juanb <mail2juanb@yahoo.fr>
2024-10-29 12:58:18 +0100

    STYLE - FirePersonsResponse

--- src/main/java/com/oc_P5/SafetyNetAlerts/dto/FirePersonsResponse.java
+++ src/main/java/com/oc_P5/SafetyNetAlerts/dto/FirePersonsResponse.java
@@ -1,26 +1,14 @@
 package com.oc_P5.SafetyNetAlerts.dto;
 
-
 import lombok.Value;
 
 import java.util.List;
-import java.util.Optional;
 
 @Value
 public class FirePersonsResponse {
 
     List<FirePersonByAddress> firePersonByAdressList;
     String address;
-    Optional<Integer> stationNumber;
-
-
-// NOTE: Devrait être supprimé car l'annotation @Value se charge de créer le constructeur
-
-//    public FirePersonsResponse(List<FirePersonByAddress> firePersonByAdressList, String address, Optional<Integer> stationNumber) {
-//        this.firePersonByAdressList = firePersonByAdressList;
-//        this.address = address;
-//        this.stationNumber = stationNumber;
-//    }
-
+    Integer stationNumber;
 
 }
